0a84e06b3ae707a29a9c2cce8138a7a64f7c151d
jonathanrichardl <47639970+jonathanrichardl@users.noreply.github.com>
2021-09-25 22:53:12 +0800

    Delete news_update.go

--- pkg/models/news_update.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type NewsUpdate struct {
-	Title  *string   `json:"title,omitempty"`
-	Topic  *string   `json:"topic,omitempty"`
-	Tags   *[]string `json:"tags,omitempty"`
-	Status *string   `json:"status,omitempty"`
-}
