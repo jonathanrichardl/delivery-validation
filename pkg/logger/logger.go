6935aa26653b237efff2128f33a582dc6695b297
jonathanrichardl <47639970+jonathanrichardl@users.noreply.github.com>
2021-10-04 22:45:19 +0800

    Update logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -12,14 +12,9 @@ type LoggerInstance struct {
 }
 
 func NewLogger(FileName string) *LoggerInstance {
-
-	file, err := os.OpenFile(FileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	InfoLogger := log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger := log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger := log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	InfoLogger := log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger := log.New(os.Stderr, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger := log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 	return &LoggerInstance{InfoLogger: InfoLogger, WarningLogger: WarningLogger, ErrorLogger: ErrorLogger}
 
 }
